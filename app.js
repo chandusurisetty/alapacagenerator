b9b7e20c7481de0696d38a8553837fdd86b5250f
Chandu <68140710+chandusurisetty@users.noreply.github.com>
2022-04-08 16:11:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -75,7 +75,8 @@ rand.addEventListener("click", () => {
 
 
 
-        change.src = `http://127.0.0.1:5500/modals/${part}/${randPart}.png`
+//         change.src = `http://127.0.0.1:5500/modals/${part}/${randPart}.png`
+        change.src = `https://chandusurisetty.github.io/alapacagenerator/modals/${part}/${randPart}.png`
 
 
 
